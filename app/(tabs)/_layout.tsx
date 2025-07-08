e97dce9f1b7d10366dd68dae774d30eba1c7201a
girum22 <girum.tilahun19@gmail.com>
2025-07-08 21:52:48 +0300

    feat: added the pages to layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -40,6 +40,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="income"
+        options={{
+          title: 'Income',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="dollarsign.circle.fill" color={color} />, // You can change the icon name if you want
+        }}
+      />
     </Tabs>
   );
 }
